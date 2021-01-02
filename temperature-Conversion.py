baeb54f0aa410bb74d4dab56bb08615dd1b7eab4
Danish <57083546+Danish-1@users.noreply.github.com>
2021-01-03 00:45:27 +0530

    My commit
    This is a general purpose temperature conversion program.

---
+++ temperature-Conversion.py
@@ -0,0 +1,9 @@
+# input temp. in celsius
+celsius = int(input())
+# celsius to fahrenheit 
+fahrenheit = celsius*1.8 + 32
+# celsius to kelvin 
+kelvin = celsius + 273.15
+# print fahrenheit and kelvin 
+print("fahrenheit : ", fahrenheit )
+print("kelvin : ", kelvin)
